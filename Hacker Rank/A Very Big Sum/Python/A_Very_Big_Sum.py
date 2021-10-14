393e762c6714f4b1768470494f6dde585a91a2e8
Dishant Israni <isranidishant14@gmail.com>
2021-10-15 00:28:01 +0530

    Added Python Solution for Few Problems

---
+++ Hacker Rank/A Very Big Sum/Python/A_Very_Big_Sum.py
@@ -0,0 +1,25 @@
+import math
+import os
+import random
+import re
+import sys
+
+def aVeryBigSum(ar):
+    sum = 0
+    for i in ar:
+        sum = sum + i
+    return sum
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
